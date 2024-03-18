83e78356d2d0e8be8d871e86df77a96c50fef8b8
Namadev <nam.dpatil@gmail.com>
2024-03-18 23:04:50 +0530

    some code is added for authorization

--- src/main/java/com/shop/modal/User.java
+++ src/main/java/com/shop/modal/User.java
@@ -39,7 +39,7 @@ public class User {
 	private String email;
 	
 	@NotEmpty
-	@Column(nullable = false)
+	@Column(nullable = false, length = 60)
 	private String password;
 	
 	@ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
